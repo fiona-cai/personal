3177818042ca7555a8ac567774929c4485e5dbe3
Fiona Cai <fiona.cai899@gmail.com>
2024-06-15 21:15:55 -0400

    swap spline component

--- components/Spline/index.js
+++ components/Spline/index.js
@@ -5,7 +5,7 @@ export default function App() {
     return (
       <div>
         <Spline
-          scene="https://prod.spline.design/woeHEybzrcDoPYFT/scene.splinecode" 
+          scene="https://prod.spline.design/L19y2WPG8QjVI-4R/scene.splinecode"
         />
       </div>
     );
