e3890367a62a866f5090e35b97fedaf45c222098
Fiona Cai <fiona.cai899@gmail.com>
2024-10-25 11:02:47 -0400

    thumbnail

--- components/Line/index.js
+++ components/Line/index.js
@@ -43,19 +43,37 @@ export default function App() {
   return (
     <div style={{ zIndex: -1, position: 'relative', overflow: 'hidden', height: '720px' }}>
       {isSmallScreen ? (
-        <video
-          ref={videoRef} // Attach the ref to the video element
-          src="/line.mp4"
-          autoPlay={videoPlaying} // Control autoPlay with the state
-          muted
-          loop={false}
-          playsInline // Ensure the video plays inline on mobile
-          style={{
-            width: '100vw',
-            height: '720px',
-            objectFit: 'cover',
-          }}
-        />
+        <>
+          {!videoPlaying && ( // Show thumbnail if video is not playing
+            <img
+              src="/thumbnail.png" // Replace with your thumbnail image source
+              alt="Video Thumbnail"
+              style={{
+                width: '100vw',
+                height: '720px',
+                objectFit: 'cover',
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                zIndex: 1, // Ensure the thumbnail is above the video
+              }}
+            />
+          )}
+          <video
+            ref={videoRef} // Attach the ref to the video element
+            src="/line.mp4"
+            autoPlay={videoPlaying} // Control autoPlay with the state
+            muted
+            loop
+            playsInline // Ensure the video plays inline on mobile
+            style={{
+              width: '100vw',
+              height: '720px',
+              objectFit: 'cover',
+              display: videoPlaying ? 'block' : 'none', // Hide video until it starts
+            }}
+          />
+        </>
       ) : (
         <Spline scene="https://prod.spline.design/L19y2WPG8QjVI-4R/scene.splinecode" />
       )}
